4160150f41c95b0dfc452bdff2a3be559de09a1b
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-06-10 18:09:11 +0500

    Advertiser-form Added

---
+++ src/advertiser/advertiser.schema.ts
@@ -0,0 +1,76 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+@Schema()
+export class Advertiser {
+
+  @Prop({ required: true })
+  advertiserName: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true})
+  email: string;
+
+  @Prop({ required: true })
+  invoicingContactName: string;
+
+  @Prop({ required: true })
+  emailForSendInvoice: string;
+
+  @Prop({ required: true })
+  invoicingMainlingAddress: string;
+
+  @Prop({ required: true })
+  primaryContact: string;
+
+  @Prop()
+  additionalContact: string;
+
+  @Prop()
+  legalDisclaimer: string;
+
+  @Prop({ required: true })
+  landingPage: string;
+
+  @Prop({ required: true })
+  UTM : string;
+
+  @Prop()
+  promoCode: string;
+
+  @Prop()
+  mainValueProposition: string;
+
+  @Prop()
+  apartFromCompetitors: string;
+
+  @Prop({ required: true })
+  specialProducts: string;
+
+  @Prop()
+  barriorToPurchase: string;
+
+  @Prop()
+  anythingToMension: string;
+
+  @Prop()
+  successfulMessage: string;
+
+  @Prop()
+  targetPerformanceMetrices: string;
+
+  @Prop()
+  logo: String;
+
+  @Prop()
+  specificUTMParameters: string;
+
+  @Prop({ required: true})
+  mediaIntercept: string;
+}
+
+export const AdvertiserSchema = SchemaFactory.createForClass(Advertiser);
+
+export type AdvertiserDocument = Advertiser & Document;
---
+++ src/advertiser/advertiser.service.ts
@@ -0,0 +1,39 @@
+// import { Injectable } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Advertiser, AdvertiserDocument } from './advertiser.schema';
+// import { CreateAdvertiserDto } from './dto/create-advertiser.dto';
+
+// @Injectable()
+// export class AdvertiserService {
+//   constructor(
+//     @InjectModel(Advertiser.name) private advertiserModel: Model<AdvertiserDocument>,
+//   ) {}
+
+//   async create(createAdvertiserDto: CreateAdvertiserDto): Promise<Advertiser> {
+//     const createdAdvertiser = new this.advertiserModel(createAdvertiserDto);
+//     return createdAdvertiser.save();
+//   }
+// }
+
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Advertiser, AdvertiserDocument } from './advertiser.schema';
+import { CreateAdvertiserDto } from './dto/create-advertiser.dto';
+
+@Injectable()
+export class AdvertiserService {
+  constructor(
+    @InjectModel(Advertiser.name) private advertiserModel: Model<AdvertiserDocument>,
+  ) {}
+
+  async create(createAdvertiserDto: CreateAdvertiserDto, filePath: string): Promise<Advertiser> {
+    const createdAdvertiser = new this.advertiserModel({
+      ...createAdvertiserDto,
+      logo: filePath,
+    });
+    return createdAdvertiser.save();
+  }
+}
