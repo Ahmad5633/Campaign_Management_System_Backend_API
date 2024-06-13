20ba05a62ac450e602bf0ca7abfc0bcc57ccd953
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-06-13 15:39:25 +0500

    Authentication removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ForgetPasswordModule } from './resetPassword/forget-password.module';
-import { LoginAuthModule } from './login/auth.module';
+// import { LoginAuthModule } from './login/auth.module';
 import { GoogleAuthModule } from './auth/auth.module';
 import { AdvertiserModule } from './advertiser/advertiser.module';
 import { PublisherModule } from './publisher/publisher.module';
@@ -14,7 +14,7 @@ import { CampaignModule } from './campaign/campaign.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
     ForgetPasswordModule,
-    LoginAuthModule,
+    // LoginAuthModule,
     GoogleAuthModule,
     AdvertiserModule,
     PublisherModule,
