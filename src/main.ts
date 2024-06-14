95a10638f1fe110233fc88408adf0d7deab81331
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-06-14 14:50:08 +0500

    Better Code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (process.env.NODE_ENV === 'production') {
     app.enableCors();
   }
-
   const config = new DocumentBuilder()
     .setTitle('Campaign Management System')
     .setDescription('This website is used to for Advertisement Purposes')
@@ -16,6 +17,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  const publicDir = path.join(__dirname, '..', 'public');
+  app.use('/api-docs', express.static(publicDir));
 
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
