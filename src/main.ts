966db2080675f4f67b732c63e60d3c4004e41c2d
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-06-14 16:52:07 +0500

    Better Code

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,46 @@
+// import { NestFactory } from '@nestjs/core';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+// import * as express from 'express';
+// import * as path from 'path';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   if (process.env.NODE_ENV === 'production') {
+//     app.enableCors();
+//   }
+//   const config = new DocumentBuilder()
+//     .setTitle('Campaign Management System')
+//     .setDescription('This website is used to for Advertisement Purposes')
+//     .setVersion('1.0')
+//     .addTag('APIs of the Compaign Management System')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   // const publicDir = path.join(__dirname, '..', 'public');
+//   // app.use('/api-docs', express.static(publicDir));
+
+//   await app.listen(process.env.PORT || 3000);
+//   console.log(`Application is running on: ${await app.getUrl()}`);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as express from 'express';
-import * as path from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV === 'production') {
-    app.enableCors();
-  }
+
   const config = new DocumentBuilder()
-    .setTitle('Campaign Management System')
-    .setDescription('This website is used to for Advertisement Purposes')
+    .setTitle('Your API')
+    .setDescription('API description')
     .setVersion('1.0')
-    .addTag('APIs of the Compaign Management System')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  const publicDir = path.join(__dirname, '..', 'public');
-  app.use('/api-docs', express.static(publicDir));
 
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
